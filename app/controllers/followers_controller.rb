ed2b729b6199cc6cc3484a12141bd21d38ab8b3f
albiebabie <albiebabie@gmail.com>
2016-10-10 19:08:41 +0100

    working locally

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,8 +1,9 @@
 class FollowersController < ApplicationController
 
   def index
-    Follower.save_to_database(followers_hash)
-    @display_followers = Follower.all
+    user_id = current_user[:id]
+    Follower.save_to_database(followers_hash, user_id)
+    @display_followers = Follower.where(:user_id => user_id)
   end
 
   private
--- app/models/follower.rb
+++ app/models/follower.rb
@@ -2,9 +2,10 @@ class Follower < ActiveRecord::Base
 
   belongs_to :user
 
-  def self.save_to_database(followers_hash)
+  def self.save_to_database(followers_hash, user_id)
     followers_hash.each do |follower|
       Follower.find_or_create_by(
+      user_id: user_id,
       uid: follower.id, name: follower.name,
       screen_name: follower.screen_name,
       url: "https://twitter.com/#{follower.screen_name}"
