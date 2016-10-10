41cfb381c31bf42d963dd9088767149a27e9c28d
albiebabie <albiebabie@gmail.com>
2016-10-10 12:33:27 +0100

    trying to deploy

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,13 +1,18 @@
 class FollowersController < ApplicationController
 
+  # def create
+    # Follower.save_to_database(followers_hash)
+    # redirect_to :action => 'index'
+  # end
+
   def index
-    Follower.save_to_database(followers)
+    Follower.save_to_database(followers_hash)
     @display_followers = Follower.all
   end
 
   private
 
-  def followers
+  def followers_hash
     @followers = client.followers
   end
 end
--- app/models/follower.rb
+++ app/models/follower.rb
@@ -2,9 +2,9 @@ class Follower < ActiveRecord::Base
 
   belongs_to :user
 
-  def self.save_to_database(followers)
-    followers.each do |follower|
-      Follower.create(
+  def self.save_to_database(followers_hash)
+    followers_hash.each do |follower|
+      Follower.find_or_create_by(
       uid: follower.id, name: follower.name,
       screen_name: follower.screen_name,
       url: "https://twitter.com/#{follower.screen_name}"
